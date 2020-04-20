ef6bc451720dc660013b580c2af6529fffba9888
guoxj <guoxj@miiteen.com>
2020-04-20 18:35:45 +0800

    init

---
+++ scalpel/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace scalpel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("TIM");
+            this.label1.Text = processes[0].MainWindowTitle;
+        }
+    }
+}
